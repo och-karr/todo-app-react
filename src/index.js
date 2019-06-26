f51086685249d37c2d6b89f46a3a42cee78623b3
karolinneczka <okaa93@gmail.com>
2019-06-26 21:30:21 +0200

    Make working app.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let id = 0;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Todo = props => (
+  <li>
+    <input type="checkbox" checked={props.todo.checked} onChange={props.onToggle}/>
+    <button onClick={props.onDelete}>delete</button>
+    <span>{props.todo.text}</span>
+  </li>
+)
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [],
+    }
+  }
+
+  addTodo() {
+    const text = prompt("TODO text please")
+    this.setState({
+      todos: [...this.state.todos, {id: id++, text: text, checked: false }],
+    })
+  }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id)
+    })
+  }
+
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id !== id) return todo
+        return {
+          id: todo.id,
+          text: todo.text,
+          checked: !todo.checked,
+        }
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Todo count: {this.state.todos.length}</div>
+        <div>Unchecked todo count: {this.state.todos.filter(todo => !todo.checked).length}</div>
+        <button onClick={() => this.addTodo()}>Add Todo</button>
+        <ul>
+          {this.state.todos.map(todo => (
+            <Todo 
+              onToggle={() => this.toggleTodo(todo.id)}
+              onDelete={() => this.removeTodo(todo.id)} 
+              todo={todo} 
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
