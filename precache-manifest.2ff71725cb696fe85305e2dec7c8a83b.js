dec98fe926bb971cdfe1f824571dc1e0f04a7b04
karolinneczka <okaa93@gmail.com>
2019-07-02 18:34:57 +0200

    Updates

---
+++ precache-manifest.2ff71725cb696fe85305e2dec7c8a83b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af872dd8d2e35c335f50a05e20c7440",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "46d15b2b2bdcdad0780f",
+    "url": "/todo-app-react/static/css/main.3c683513.chunk.css"
+  },
+  {
+    "revision": "f0a0a6c2215a2986b69a",
+    "url": "/todo-app-react/static/js/2.73230106.chunk.js"
+  },
+  {
+    "revision": "46d15b2b2bdcdad0780f",
+    "url": "/todo-app-react/static/js/main.98040482.chunk.js"
+  },
+  {
+    "revision": "8ed63381bb10a3952e6a",
+    "url": "/todo-app-react/static/js/runtime~main.ec7f1ccf.js"
+  }
+]);
